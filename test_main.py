2142fec88fdc29155b86cfabf3d7b7caff6de30b
praachipawar <praachipawar7@gmail.com>
2025-05-14 15:51:16 +0530

    fourthcommit

---
+++ test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app          # import your FastAPI instance
+
+client = TestClient(app)
+
+def test_root():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"status": "ok"}
